0250130184a2ff8651cbb934cd948c4da7145887
minahh <38228523+minahh@users.noreply.github.com>
2018-04-10 03:28:23 +0500

    NEW

--- oscillator.js
+++ oscillator.js
@@ -1,20 +1,48 @@
-var time = 0;
-
 function oscillator() {
-  this.update = function() {
-    this.x = width/2;
-    this.y = 0;
-    var freq = 6;
-    var amp = 150;
-    amp = amp * exp(-0.01*time);
-    time++;
-    this.x += time;
-    this.y = cos(time * freq) * amp;
 
+  this.x = 0;
+  this.y = 0;
+  this.history = [];
+  var time = 0;
+  var xoff = 0;
+  var inc = 0.001;
+  var frequency;
+  amplitude = 150;
+
+  this.update = function(force, frequency) {
+    this.x = noise(xoff) * 20;
+    xoff = xoff + inc;
+    time = time + 1;
+    this.y = cos(frequency * time) * amplitude * force;
   }
 
-  this.show = function() {
-    fill(51);
-    ellipse(this.x,this.y,6,6);
+  this.show = function(size) {
+    noStroke();
+    fill(90);
+    ellipse(this.x, this.y, size, size);
+    stroke(255, 170, 10);
+    var sw = dist(0, -height, this.x, this.y);
+    sw = 5500 / sw;
+    strokeWeight(sw);
+    line(0, -height, this.x, this.y - 25);
   }
+
+  this.dampen = function(alpha) {
+    if (amplitude > 0.1) {
+      amplitude = amplitude * exp(-alpha * time);
+    } else {
+      amplitude = 0;
+      inc = 0;
+    }
+  }
+
+  this.time = time;
+  this.history.push(this.x);
+
+  // this.trace = function() {
+  //   noStroke();
+  //   fill(0, 255, 100);
+  //   ellipse(this.x + time, this.y, 5, 5);
+  // }
+
 }
--- sketch.js
+++ sketch.js
@@ -2,17 +2,23 @@ let width = 700;
 let height = 600;
 
 var bob;
+var tracer;
 
 function setup() {
   createCanvas(width, height);
   bob = new oscillator();
+  tracer = new tracer();
   angleMode(DEGREES);
-  //console.log(bob.amp);
-  background(255);
 }
 
 function draw() {
-  translate(0,300);
-  bob.update(); //FORCE BETWEEN 0 AND 1, FREQ
-  bob.show(); //BOB SIZE
+  background(255);
+  stroke(240);
+  strokeWeight(2);
+  translate(width / 2, height / 2);
+  line(-width / 2, 0, width / 2, 0);
+  bob.update(0.8, 8); //FORCE BETWEEN 0 AND 1, FREQ
+  bob.dampen(0.0001); //ALPHA MORE ZEROS LESS DAMPING
+  bob.show(50); //BOB SIZE
 }
+
