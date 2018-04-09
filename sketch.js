859ef77b8e4923a000fd5dee03ad1dcd688ef974
minahh <38228523+minahh@users.noreply.github.com>
2018-04-10 03:43:30 +0500

    colour

--- sketch.js
+++ sketch.js
@@ -1,24 +1,34 @@
-let width = 700;
-let height = 600;
+var inc = 0.005;
+var xoff0 = 100;
+var xoff1 = 1400;
+var xoff2 = 3000;
 
-var bob;
-var tracer;
 
 function setup() {
+  var width = windowWidth / 1.5;
+  var height = windowHeight / 1.5;
+  startRed = width / 4;
+  startGreen = 2 * startRed;
+  startBlue = 3 * startRed;
   createCanvas(width, height);
-  bob = new oscillator();
-  tracer = new tracer();
-  angleMode(DEGREES);
 }
 
 function draw() {
-  background(255);
-  stroke(240);
-  strokeWeight(2);
-  translate(width / 2, height / 2);
-  line(-width / 2, 0, width / 2, 0);
-  bob.update(0.8, 8); //FORCE BETWEEN 0 AND 1, FREQ
-  bob.dampen(0.0001); //ALPHA MORE ZEROS LESS DAMPING
-  bob.show(50); //BOB SIZE
+  xoff0 = xoff0 + inc;
+  xoff1 = xoff1 + inc;
+  xoff2 = xoff2 + inc;
+  var red = floor(noise(xoff0) * 255);
+  var green = floor(noise(xoff1) * 255);
+  var blue = floor(noise(xoff2) * 255);
+  background(red, green, blue);
+  blueSize = map(blue, 0, 255, 0, width / 3);
+  redSize = map(red, 0, 255, 0, width / 3);
+  greenSize = map(green, 0, 255, 0, width / 3);
+  noStroke();
+  fill(255, 100, 100);
+  ellipse(startRed, height / 2, redSize, redSize);
+  fill(20, 255, 100);
+  ellipse(startGreen, height / 2, greenSize, greenSize);
+  fill(20, 150, 255);
+  ellipse(startBlue, height / 2, blueSize, blueSize);
 }
-
