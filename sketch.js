3c76bf8f0f3aaf46f939f0701f4e5e8cc30de3c2
minahh <38228523+minahh@users.noreply.github.com>
2018-04-15 05:13:55 +0500

    SImple

--- sketch.js
+++ sketch.js
@@ -1,34 +1,35 @@
-var inc = 0.005;
-var xoff0 = 100;
-var xoff1 = 1400;
-var xoff2 = 3000;
+let fulhaamin = 355;
+let iskolhu = 300;
 
+var boalha;
+var fattaa = 0;
+var angleSlider;
 
 function setup() {
-  var width = windowWidth;
-  var height = windowHeight;
-  startRed = width / 4;
-  startGreen = 2 * startRed;
-  startBlue = 3 * startRed;
-  createCanvas(width, height);
+  createCanvas(fulhaamin, iskolhu);
+  angleMode(DEGREES);
+  boalha = new heleyBoalha(160, 50);
+  angleSlider = createSlider(-55,55,-30);
+  angleSlider.position(40,120);
 }
 
 function draw() {
-  xoff0 = xoff0 + inc;
-  xoff1 = xoff1 + inc;
-  xoff2 = xoff2 + inc;
-  var red = floor(noise(xoff0) * 255);
-  var green = floor(noise(xoff1) * 255);
-  var blue = floor(noise(xoff2) * 255);
-  background(red, green, blue);
-  blueSize = map(blue, 0, 255, 0, width / 3);
-  redSize = map(red, 0, 255, 0, width / 3);
-  greenSize = map(green, 0, 255, 0, width / 3);
-  noStroke();
-  fill(255, 101, 100);
-  ellipse(startRed, height / 2, redSize, redSize);
-  fill(20, 255, 100);
-  ellipse(startGreen, height / 2, greenSize, greenSize);
-  fill(20, 150, 255);
-  ellipse(startBlue, height / 2, blueSize, blueSize);
+  if (fattaa == 0) {
+    boalha.angle = angleSlider.value();
+  }
+  background(255);
+  translate(width / 2, 40);
+  boalha.clampKurahaa();
+  boalha.dhakkaa();
+  boalha.physicsUpdateKurey();
+  boalha.resistKurey();
+  if (fattaa == 1) {
+    boalha.oscillateKurey();
+  }
+}
+
+function keyPressed() {
+  if (keyCode == 32) {
+    fattaa = 1;
+  }
 }
