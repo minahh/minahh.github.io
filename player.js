29697a5e3c599e59cae65e08da5c4655ddd1fcd5
minahh <38228523+minahh@users.noreply.github.com>
2018-04-20 00:37:04 +0500

    123

--- player.js
+++ player.js
@@ -1,24 +1,12 @@
 function Saina(size) {
 
-  var inc = 0.004;
-  var xoff0 = 100;
-  var xoff1 = 1400;
-  var xoff2 = 3000;
-
   this.x = width / 2;
   this.y = height - groundHeight - (size / 2);
 
   this.vel = 0;
 
   this.show = function() {
-    xoff0 += inc;
-    xoff1 += inc;
-    xoff2 += inc;
-    var red = floor(noise(xoff0) * 255);
-    var green = floor(noise(xoff1) * 255);
-    var blue = floor(noise(xoff2) * 255);
-    fill(red, green, blue);
-    //fill(0,172,193);
+    fill(0,172,193);
     //fill(65, 217, 244);
     ellipse(this.x, this.y, size, size);
   }
